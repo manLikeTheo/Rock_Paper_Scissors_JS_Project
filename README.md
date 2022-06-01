f0cad16383e34d7b2659ca24963f7ce9c6cdf424
Theophilus Adewale <adewaletheophilus@gmail.com>
2022-06-02 00:46:50 +0100

    Add Three Buttons to the index.html file

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Building the "Rock-Paper-Scissors" game using the skills i've learnt following "
 I must say this is a very simple and "well-structured" course, so kudos to the guys that put this together.
 For now, we're only going to play this game in the console so we only get to test our javascript logic in the browser console
 Look forward to improving this project in the no distant time as i continue with the foundations course
+
+Okay! Revisiting this project to add a bit of UI functionality was both challenging and interesting at the same time. 
+Got to practice much of what i learned in the DOM manipulation section. Totally worth the long hours i spent starign at my PC screen like a knobhead cause i got stuck. Moving On to more interesting and challenging lessons.
--- main.js
+++ main.js
@@ -6,56 +6,130 @@ function computerPlay(){
 }
 
 
-//Player's choice
-function playerChoice(){
-    let input = prompt("Make your move!")
-    while(input === null){
-        input = prompt("Please, make your move!");
-    }
-    input = input.toLowerCase();
-    return input;
-}
-
-//Validate input
-function validateInput(choice){
-    if(random.includes(choice)){
-        return true;
-    } else{
-        return false;
-    }
-}
-
 //Global variables
 let playerScore = 0;
 let computerScore = 0;
 
 //To play the Game
 function game(){
-    let gameWinner = "";
-    //Loop for 5 rounds
-    let rounds = 0;
-    for(let i = rounds; i < 5; i++){
-    //log selections
-    const playerSelection = playerChoice();
-    console.log("Player selects: " + playerSelection);
-    const computerSelection = computerPlay();
-    console.log("Computer selects: " + computerSelection);
-    //Call the playRound function inside the main game
-    playRound(playerSelection, computerSelection);
+
+// DOM -selectors
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.style.fontSize = "25px";
+    button.style.background = "lightblue";
+    button.style.borderRadius = "7px";
+    button.addEventListener("click", () => {
+        playerSelection = button.innerHTML;
+
+        playerChoice.innerHTML = "Player selects: " + playerSelection;
+        const computerSelection = computerPlay();
+        computerChoice.innerHTML = "Computer selects: " + computerSelection;
+        //Call the playRound function inside the main game
+        gameRally.textContent = playRound(playerSelection, computerSelection);
+        let gameWinner = "";
+        trackPlayerScore.textContent = "Player Score = " + playerScore;
+        trackComputerScore.textContent = "Computer Score = " + computerScore;
+        resetGame();
+    })
+})
+    
+    //Function to determine Winner
+    //1st player to score 5 points wins
+    function resetGame(){
+        if(playerScore == 5){
+            gameWinner = "YOU WIN!";
+            gameWinnerText.textContent = gameWinner;
+
+            //Play Again Button will reset the game
+            playAgainBtn.style.display = "inline-block";
+            playAgainBtn.style.background = "red";
+            playAgainBtn.style.color = "#fff";
+            playAgainBtn.style.transition = "2s fade-in-out";
+            //When Clicked, it refreshes the page
+            playAgainBtn.addEventListener("click", ()=>{
+                playAgainBtn.style.background = "orange";
+                playAgainBtn.style.color = "flex";
+                window.location.reload();
+            })
+        }else if(computerScore == 5){
+            gameWinner = "YOU LOST!";
+            gameWinnerText.textContent = gameWinner;
+
+            //Create a play Again Button that will reset the game
+            playAgainBtn.style.display = "inline-block";
+            playAgainBtn.style.background = "red";
+            playAgainBtn.style.color = "#fff";
+            playAgainBtn.style.transition = "2s fade-in-out";
+            //When Clicked, it refreshes the page
+            playAgainBtn.addEventListener("click", ()=>{
+                playAgainBtn.style.background = "orange";
+                playAgainBtn.style.color = "black";
+                window.location.reload();
+            })
+        }
     }
-    //Deter,ine the winner at the end of 5 rounds
-    if(playerScore === computerScore){
-        gameWinner = "Its a TIE!";
-    } else if(playerScore < computerScore){
-        gameWinner = "You LOST!";
-    } else{
-        gameWinner = "You WIN!"
-    }
-    console.log("Winner At The End Of 5 rounds is: " + gameWinner);
+    
 }
 
 game()
 
+
+
+// DOM Selectors
+//container
+const container = document.querySelector(".container");
+
+//Play Again Button will reset the game
+const playAgainBtn = document.querySelector(".play-again");
+playAgainBtn.style.display = "none";
+
+//Results Div
+const resultsDiv = document.createElement("div");
+resultsDiv.style.border = "2px solid black";
+container.appendChild(resultsDiv);
+
+
+//player Selection
+const playerChoice = document.createElement("p");
+playerChoice.style.color = "red";
+playerChoice.style.fontSize = "25px";
+resultsDiv.appendChild(playerChoice);
+
+//computer selection
+const computerChoice = document.createElement("p");
+computerChoice.style.color = "blue";
+computerChoice.style.fontSize = "25px";
+resultsDiv.appendChild(computerChoice);
+
+//Player Score
+const trackPlayerScore = document.createElement("p");
+trackPlayerScore.style.fontSize = "22px";
+trackPlayerScore.style.color = "black";
+resultsDiv.appendChild(trackPlayerScore);
+
+//Computer Score
+const trackComputerScore = document.createElement("p");
+trackComputerScore.style.color = "black";
+trackComputerScore.style.fontSize = "22px";
+resultsDiv.appendChild(trackComputerScore);
+
+//Display Game Rally
+const gameRally = document.createElement("p");
+gameRally.style.fontWeight = "700";
+gameRally.style.fontSize =  "28";
+gameRally.style.color = "green";
+resultsDiv.appendChild(gameRally);
+
+//Create Game winner Text
+const gameWinnerText = document.createElement("p");
+gameWinnerText.style.color = "red";
+gameWinnerText.style.fontSize = "35px";
+gameWinnerText.style.transition = "all 1s ease-in-out";
+resultsDiv.appendChild(gameWinnerText);
+
+
+//Function that plays 1 round of the game
 function playRound(playerSelection, computerSelection){
     let playerWin = "You Win!";
     let computerWin = "You Lose!";
@@ -73,5 +147,4 @@ function playRound(playerSelection, computerSelection){
     }
     
 }
-
 
